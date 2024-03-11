df7235f19de6a81bdea092d865663b0f3722ec6f
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-03-10 21:26:22 -0600

    Revert "Revert "xd""
    This reverts commit 15fc38b15ff528f441716fe640e2ce2991562eed.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # labb6
  
+## main.js
+endpoints, validaciones, funciones para el blog, llama a las funciones de db.js para: 
+GET /posts que retorna un listado de todos los posts. 
+GET /posts/:postId que retorna el detalle de un post con el id postId.
+POST /posts que permite crear un nuevo post. 
+PUT /posts/:postId  
+DELETE /posts/:postId que borra un post. 
+
+
+## db.js
+funciones con los querys para obtener todos los post, crear post, get post by id, update post y delete.
+
+
+## conn.js
+conexion a la base de datos
+
+## package.json
+el json literal jaja, btw esta en modo commonjs, entonces no se pueden usar imports
+
+## Carpeta: comandos xd
+archivos:
+- comandos: comandos generales usados y para generar una aplicacion tipo json
+- esslint: comandos de eslint
+- swagger: comandos de swagger 
--- main.js
+++ main.js
@@ -1,31 +1,40 @@
 
-//se usa este en vez del otro proque hay uno que json que define elcoso y es: import express from 'express';
-//const express = require('express'); //aca installamos express
-//const app = express();
-const port = 3001;
-//const db = require('./db.js');
+/*import express from 'express'
+import * as db from './db.js'
+import swaggerUi from 'swagger-ui-express'
+import yaml from 'js-yaml'*/
 
-//uso esto xq lo otro eslint no me dejo :(
-import express from 'express';
-const app = express();
-import * as db from './db.js';
+const express = require('express')
+const db = require('./db.js')
+const swaggerUi = require('swagger-ui-express')
+const yaml = require('js-yaml')
 
+const app = express()
+
+//cargar el archivo YAML que describe endpoints
+const swaggerDocument = yaml.load('./swagger.yaml')
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+//validacion de implementacion de http 
+app.use((req, res, next) => {
+  res.status(501).send('error 501: metodo no implementad')
+})
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Express!');
-  console.log('Hello World from Express!');
-});
+  res.send('Hello World from Express!')
+  console.log('Hello World from Express!')
+})
 
-
-
-
+//middleware para json Aca es donde se usa los cors
 app.use(express.json())
+
+
 //--get all posts
 app.get('/posts', async (req, res) => {
   const posts = await db.getAllPosts()
   res.json(posts)
-  res.status(200).json(posts);
-  console.log('posts', posts);
+  res.status(200).json(posts)
+  console.log('posts', posts)
 })
 
 //--create post
@@ -34,7 +43,7 @@ app.post('/posts', async (req, res) => {
     const { title, content, titulo, descripcion, imagen } = req.body
     const newPost = await db.createPost(title, content, titulo, descripcion, imagen)
     res.json({ message: 'Post created' })
-    res.status(200).json(newPost);
+    res.status(200).json(newPost)
 
     console.log('Post created')
   } catch (error) {
@@ -45,38 +54,40 @@ app.post('/posts', async (req, res) => {
 
 //-get post by id
 app.get('/posts/:postId', async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   const post = await db.getPostId(id)
-  res.status(200).json(post);
-  //console.log('posts', posts);
+  res.status(200).json(post)
+  //console.log('posts', posts)
 })
 
 //--update post
 app.put('/posts/:postId', async (req, res) => {
-  const { id } = req.params;
-  const { title, content, titulo, descripcion, imagen } = req;
+  const { id } = req.params
+  const { title, content, titulo, descripcion, imagen } = req
   const updatedPost = await db.updatePost(id, title, content, titulo, descripcion, imagen)
   if (!updatedPost) {
     res.status(400).json({ message: 'Post not found' })
   } else {
-    res.status(200).json(updatedPost);
+    res.status(200).json(updatedPost)
     res.json({ message: 'Post updated' })
   }
 
 })
 //--delete post
 app.delete('/post/:postId', async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   const deletedPost = await db.deletePost(id)
-  res.status(204).json(deletedPost);
+  res.status(204).json(deletedPost)
   res.json({ message: 'Post deleted' })
   console.log('Post deleted')
 
 })
 
 
+//inicio del server
+const port = 3001
 app.listen(port, () => {
-  //console.log(`Example app listening at http://localhost:${port}`);
-  console.log(`Server listening at http://127.0.0.1:${port}`);
+  //console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server listening at http://127.0.0.1:${port}`)
 
-});
+})
